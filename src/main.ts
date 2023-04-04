cb1e02e4c13935071050c85c3300215bf718ba04
Sophia <beyondthesophia@gmail.com>
2023-04-05 08:15:58 +0900

    fix: now graphql api input will respect validation configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import "reflect-metadata";
 
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+
 import { AppModule } from "@root/app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+
     await app.listen(3000);
 }
 

186f50c8aacb6d06c70345b0a586f5e5061b9f45
Sophia <beyondthesophia@gmail.com>
2023-04-07 21:19:21 +0900

    test: add test case for missed coverages

--- src/common/object.service.spec.ts
+++ src/common/object.service.spec.ts
@@ -35,7 +35,7 @@ describe("ObjectService", () => {
         expect(service["searchCache"]["timeToLive"]).toBe(3600);
     });
 
-    it("should return cached results if they are available", async () => {
+    it("should return cached results for searching if they are available", async () => {
         const service = new MockService();
         const left = await service.search({
             query: "test",
@@ -50,5 +50,22 @@ describe("ObjectService", () => {
         });
 
         expect(left[0] === right[0]).toBe(true);
+    });
+
+    it("should return cached results for resolving items if they are available", async () => {
+        const service = new MockService();
+        const [left] = await service.getItems(["mocked::test"], "en");
+        const right = await service.getItem("mocked::test", "en");
+        const [right2] = await service.getItems(["mocked::test"], "en");
+
+        expect(left === right).toBe(true);
+        expect(left === right2).toBe(true);
+    });
+
+    it("should return null if given id is not valid for any resolver", async () => {
+        const service = new MockService();
+        const result = await service.getItem("test", "en");
+
+        expect(result).toBeNull();
     });
 });
